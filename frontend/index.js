8baeac220eadbe0594ba21515e43954df266de73
wenjie <pohwj24@gmail.com>
2023-04-29 23:13:26 +0800

    change to lambda url

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch ("https://pb5qhttllromby5rtixlmxtxae0avrjz.lambda-url.ap-southeast-1.on.aws/");
+    let response = await fetch ("https://unnbs7fvskee3dhtbeydbvk5fu0yenqq.lambda-url.ap-southeast-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = `Views: ${data}`;
 }
