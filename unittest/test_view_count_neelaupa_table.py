c9f5b4dc437c709b710a0687e9e2baa250877ad8
wenjie <pohwj24@gmail.com>
2023-06-29 14:11:41 +0800

    test-python-lambda

---
+++ unittest/test_view_count_neelaupa_table.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from view_count_neelaupa_table import lambda_handler  
+
+class TestLambdaHandler(unittest.TestCase):
+    @patch('view_count_neelaupa_table.table')
+    def test_lambda_handler(self, mock_table):
+        mock_table.get_item.return_value = {
+            'Item': {
+                'id': '0',
+                'views': 1
+            }
+        }
+        mock_table.put_item.return_value = {}
+        expected = 2
+        actual = lambda_handler({}, {})
+        self.assertEqual(expected, actual)
+        mock_table.get_item.assert_called_once_with(Key={'id': '0'})
+        mock_table.put_item.assert_called_once_with(Item={'id': '0', 'views': 2})
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
