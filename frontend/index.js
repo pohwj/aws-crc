3554b6af82efa2117443faad832f9be363c67011
wenjie <pohwj24@gmail.com>
2023-07-24 21:58:03 +0800

    make changes to index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch ("https://yivqqmoszglhobwv43q637w7am0oyrqg.lambda-url.ap-southeast-1.on.aws/");
+    let response = await fetch ("https://ahzt3bhhwy4owjhprrrjef7gmu0ajeby.lambda-url.ap-southeast-1.on.aws/");
     let data = await response.json();
     counter.innerHTML = `Views: ${data}`;
 }
