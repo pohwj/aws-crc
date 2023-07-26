9417a4c6818332488dd1712b0323d7420971b65c
wenjie <pohwj24@gmail.com>
2023-07-26 17:13:46 +0800

    formatted main.tf file

--- infra/main.tf
+++ infra/main.tf
@@ -7,33 +7,33 @@ terraform {
   }
 }
 
-provider "aws"{
-    region = "ap-southeast-1"   
+provider "aws" {
+  region = "ap-southeast-1"
 }
 
 resource "aws_s3_bucket" "neelaupa_bucket" {
 
-    bucket = "neelaupa-bucket-wenjie-02"
-    tags = {
-        Name = "nlp bucket"
-        Environment = "dev"
-    }
-} 
+  bucket = "neelaupa-bucket-wenjie-02"
+  tags = {
+    Name        = "nlp bucket"
+    Environment = "dev"
+  }
+}
 
 resource "aws_dynamodb_table" "my_new_table" {
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key     = "id"
-    name         = "neelaupa_table"
-    attribute {
-        name = "id"
-        type = "S"
-    }
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  name         = "neelaupa_table"
+  attribute {
+    name = "id"
+    type = "S"
+  }
 }
 
 resource "aws_dynamodb_table_item" "my_items" {
-    table_name = aws_dynamodb_table.my_new_table.name
-    hash_key = aws_dynamodb_table.my_new_table.hash_key
-    item = <<EOF
+  table_name = aws_dynamodb_table.my_new_table.name
+  hash_key   = aws_dynamodb_table.my_new_table.hash_key
+  item       = <<EOF
 
     {
         "id" : {"S" : "0"},
@@ -53,7 +53,7 @@ resource "aws_iam_role" "terraform_lambda" {
       {
         Action = "sts:AssumeRole"
         Effect = "Allow"
-        Sid = ""
+        Sid    = ""
         Principal = {
           Service = "lambda.amazonaws.com"
         }
@@ -67,7 +67,7 @@ resource "aws_iam_policy" "iam_policy_for_crc" {
   name        = "aws_iam_policy_for_terraform_resume_project_policy"
   path        = "/"
   description = "AWS IAM Policy for managing the resume project role"
-    policy = jsonencode(
+  policy = jsonencode(
     {
       Version : "2012-10-17",
       Statement : [
@@ -84,7 +84,7 @@ resource "aws_iam_policy" "iam_policy_for_crc" {
           Effect : "Allow"
           Action : [
             "dynamodb:UpdateItem",
-			      "dynamodb:GetItem",
+            "dynamodb:GetItem",
             "dynamodb:PutItem"
           ]
           Resource = [aws_dynamodb_table.my_new_table.arn]
@@ -116,7 +116,7 @@ resource "aws_lambda_function_url" "test_neelaupa" {
   function_name      = aws_lambda_function.count.function_name
   authorization_type = "NONE"
 
-  cors{
-    allow_origins     = ["https://resume.woshipwj.click"]
+  cors {
+    allow_origins = ["https://resume.woshipwj.click"]
   }
 }
